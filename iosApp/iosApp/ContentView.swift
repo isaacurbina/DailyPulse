3f53e8ab9e83ec1cf9516dec6cc7afc48afdd9bc
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-07-09 17:04:25 -0500

    11. Hello world iOS part 1

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -2,17 +2,12 @@ import SwiftUI
 import shared
 
 struct ContentView: View {
-
-    var body: some View {
-        ComposeView().ignoresSafeArea(.keyboard)
-    }
+	
+	
 }
 
-struct ComposeView: UIViewControllerRepresentable {
-    
-    func makeUIViewController(context: Context) ->  UIViewController {
-        MainIOSKt.MainViewController()
-    }
-    
-    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {}
+struct ContentView_Previews: PreviewProvider {
+	static var previews: some View {
+		ContentView()
+	}
 }
