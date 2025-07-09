8d0942de1b2570793aa4275169bea0c5aff8717e
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-07-09 16:49:47 -0500

    10. Hello world Android

--- shared/src/commonMain/kotlin/com/petros/efthymiou/dailypulse/ui/ComposeMultiplatformApp.kt
+++ shared/src/commonMain/kotlin/com/petros/efthymiou/dailypulse/ui/ComposeMultiplatformApp.kt
@@ -1,23 +1,19 @@
 package com.petros.efthymiou.dailypulse.ui
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import cafe.adriel.voyager.navigator.Navigator
-import cafe.adriel.voyager.transitions.SlideTransition
-import com.petros.efthymiou.dailypulse.ui.screens.ArticlesScreen
 
 @Composable
 fun App() {
-    MyApplicationTheme {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
-        ) {
-            Navigator(ArticlesScreen()) { navigator ->
-                SlideTransition(navigator)
+    MaterialTheme {
+        Scaffold {
+            Box(modifier = Modifier.fillMaxSize()) {
+                Text("Hello world")
             }
         }
     }
