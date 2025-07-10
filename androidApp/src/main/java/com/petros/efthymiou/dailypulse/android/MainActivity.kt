e1d185c1a0855da5d41e92a7f4c7095ae0919182
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-07-10 13:48:43 -0500

    17. Porting the About screen (#8)

--- androidApp/src/main/java/com/petros/efthymiou/dailypulse/android/MainActivity.kt
+++ androidApp/src/main/java/com/petros/efthymiou/dailypulse/android/MainActivity.kt
@@ -4,24 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.petros.efthymiou.dailypulse.ui.App
 import com.petros.efthymiou.dailypulse.ui.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
 
-        setContent {
-            MyApplicationTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    App()
-                }
-            }
-        }
-    }
+		setContent {
+			MyApplicationTheme {
+				Surface(
+					modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+				) {
+					App()
+				}
+			}
+		}
+	}
 }
--- shared/src/commonMain/kotlin/com/petros/efthymiou/dailypulse/ui/ComposeMultiplatformApp.kt
+++ shared/src/commonMain/kotlin/com/petros/efthymiou/dailypulse/ui/ComposeMultiplatformApp.kt
@@ -4,16 +4,16 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.petros.efthymiou.dailypulse.ui.screens.AboutScreen
 
 @Composable
 fun App() {
     MaterialTheme {
         Scaffold {
             Box(modifier = Modifier.fillMaxSize()) {
-                Text("Hello world")
+                AboutScreen()
             }
         }
     }
