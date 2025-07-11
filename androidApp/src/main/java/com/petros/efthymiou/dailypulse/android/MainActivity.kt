dcd49e105ae5822cb3e443eba50e82bf981b675b
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-07-11 17:30:42 -0500

    23. Testing the Articles screen

--- androidApp/src/main/java/com/petros/efthymiou/dailypulse/android/MainActivity.kt
+++ androidApp/src/main/java/com/petros/efthymiou/dailypulse/android/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.petros.efthymiou.dailypulse.android
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -23,5 +24,10 @@ class MainActivity : ComponentActivity() {
 				}
 			}
 		}
+	}
+
+	override fun onResume() {
+		super.onResume()
+		Log.d("MainActivity", "onResume()")
 	}
 }
