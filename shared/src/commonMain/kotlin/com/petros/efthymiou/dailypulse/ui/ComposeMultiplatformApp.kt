651e4ed84f29db059ffe22c6efaa454b9661e0e1
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-07-11 17:34:36 -0500

    24. Application structure

--- shared/src/commonMain/kotlin/com/petros/efthymiou/dailypulse/ui/ComposeMultiplatformApp.kt
+++ shared/src/commonMain/kotlin/com/petros/efthymiou/dailypulse/ui/ComposeMultiplatformApp.kt
@@ -4,17 +4,24 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.petros.efthymiou.dailypulse.ui.screens.ArticlesScreen
 
 @Composable
 fun App() {
-    MaterialTheme {
+    MyApplicationTheme {
+		Surface(
+			modifier = Modifier.fillMaxSize(),
+			color = MaterialTheme.colorScheme.background
+		) {
+
+		}
         Scaffold {
             Box(modifier = Modifier.fillMaxSize()) {
-                ArticlesScreen()
-            }
+				Text("Hello world")
+			}
         }
     }
 }
