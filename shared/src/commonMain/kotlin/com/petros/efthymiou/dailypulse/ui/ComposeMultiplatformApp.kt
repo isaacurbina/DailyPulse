7f730fc7121a923d3325e25624064c0fcdbd18cd
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-07-14 12:26:33 -0500

    25. Integrate the Voyager library

--- shared/src/commonMain/kotlin/com/petros/efthymiou/dailypulse/ui/ComposeMultiplatformApp.kt
+++ shared/src/commonMain/kotlin/com/petros/efthymiou/dailypulse/ui/ComposeMultiplatformApp.kt
@@ -8,6 +8,8 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import cafe.adriel.voyager.navigator.Navigator
+import com.petros.efthymiou.dailypulse.ui.screens.ArticlesScreen
 
 @Composable
 fun App() {
@@ -16,7 +18,7 @@ fun App() {
 			modifier = Modifier.fillMaxSize(),
 			color = MaterialTheme.colorScheme.background
 		) {
-
+			Navigator(ArticlesScreen())
 		}
         Scaffold {
             Box(modifier = Modifier.fillMaxSize()) {
