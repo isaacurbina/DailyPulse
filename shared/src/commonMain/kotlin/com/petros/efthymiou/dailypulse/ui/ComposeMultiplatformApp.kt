a9b26dacb3139b46571ae3aded31b8aaaf626698
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-07-14 13:22:42 -0500

    30_Screen-transition-animations

--- shared/src/commonMain/kotlin/com/petros/efthymiou/dailypulse/ui/ComposeMultiplatformApp.kt
+++ shared/src/commonMain/kotlin/com/petros/efthymiou/dailypulse/ui/ComposeMultiplatformApp.kt
@@ -1,29 +1,24 @@
 package com.petros.efthymiou.dailypulse.ui
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.navigator.Navigator
+import cafe.adriel.voyager.transitions.SlideTransition
 import com.petros.efthymiou.dailypulse.ui.screens.ArticlesScreen
 
 @Composable
 fun App() {
-    MyApplicationTheme {
+	MyApplicationTheme {
 		Surface(
 			modifier = Modifier.fillMaxSize(),
 			color = MaterialTheme.colorScheme.background
 		) {
-			Navigator(ArticlesScreen())
-		}
-        Scaffold {
-            Box(modifier = Modifier.fillMaxSize()) {
-				Text("Hello world")
+			Navigator(ArticlesScreen()) { navigator ->
+				SlideTransition(navigator)
 			}
-        }
-    }
+		}
+	}
 }
