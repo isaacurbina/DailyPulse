a6a7317f9cb05fb1e6b58a3059c8be784903d9fc
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-07-16 18:06:51 -0500

    41. Desktop main class

--- desktop/src/jvmMain/kotlin/Main.kt
+++ desktop/src/jvmMain/kotlin/Main.kt
@@ -1,2 +1,26 @@
-class Main {
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+import com.petros.efthymiou.dailypulse.di.initKoin
+import com.petros.efthymiou.dailypulse.ui.App
+
+fun main() {
+	initKoin()
+
+	application {
+		val windowsState = rememberWindowState()
+
+		Window(
+			onCloseRequest = ::exitApplication,
+			state = windowsState,
+			title = "Daily Pulse"
+		) {
+			Surface(modifier = Modifier.fillMaxSize()) {
+				App()
+			}
+		}
+	}
 }
