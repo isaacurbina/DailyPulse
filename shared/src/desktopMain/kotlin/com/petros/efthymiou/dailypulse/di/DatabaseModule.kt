a8c1cfaa659ab37a88a1ba397fb370969ffb466e
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-07-17 16:14:58 -0500

    47. SQL driver factory

--- shared/src/desktopMain/kotlin/com/petros/efthymiou/dailypulse/di/DatabaseModule.kt
+++ shared/src/desktopMain/kotlin/com/petros/efthymiou/dailypulse/di/DatabaseModule.kt
@@ -6,6 +6,6 @@ import org.koin.dsl.module
 import petros.efthymiou.dailypulse.db.DailyPulseDatabase
 
 val databaseModule = module {
-	single<SqlDriver> { DatabaseDriverFactory().createDriver() }
+	single<SqlDriver> { DatabaseDriverFactory().createDriver()!! }
 	single<DailyPulseDatabase> { DailyPulseDatabase(get()) }
 }
