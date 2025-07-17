e5bc1956b0ccc5c7464312759deb129e88a2f9bc
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-07-17 16:03:49 -0500

    46. Platform Implementation

---
+++ shared/src/jsMain/kotlin/com/petros/efthymiou/dailypulse/Platform.js.kt
@@ -0,0 +1,17 @@
+package com.petros.efthymiou.dailypulse
+
+actual class Platform {
+	actual val osName: String
+		get() = "Web"
+	actual val osVersion: String
+		get() = "Web"
+	actual val deviceModel: String
+		get() = "Web"
+	actual val density: Int
+		get() = 0
+
+	actual fun logSystemInfo() {
+		println("($osName, $osVersion, $deviceModel, $density)")
+	}
+
+}
