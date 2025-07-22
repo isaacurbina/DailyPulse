0d31ef484b3fdff9dedd9fcd9a111aec33dc0739
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-07-22 15:30:07 -0500

    52. Web build gradle part 2 (#36)

--- web/build.gradle.kts
+++ web/build.gradle.kts
@@ -2,3 +2,26 @@ plugins {
 	alias(libs.plugins.kotlinMultiplatform)
 	alias(libs.plugins.composeMultiplatformPlugin)
 }
+
+kotlin {
+	js(IR) {
+		browser {
+			commonWebpackConfig {
+				outputFileName = "DailyPulse.js"
+			}
+		}
+		binaries.executable()
+	}
+
+	sourceSets {
+		jsMain.dependencies {
+			implementation(projects.shared)
+			implementation(compose.runtime)
+			implementation(compose.foundation)
+			implementation(compose.ui)
+			implementation(compose.material)
+			implementation(compose.material3)
+			implementation(libs.koin.core)
+		}
+	}
+}
